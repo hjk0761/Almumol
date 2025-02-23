c577a28157f5e484fdaa1b066dfb3f55a59bc37d
이낙헌 <95845037+nak-honest@users.noreply.github.com>
2025-02-23 17:37:19 +0900

    Create 돌_그룹(12886).py

---
+++ nak-honest/Baekjoon/GOLD/돌_그룹(12886).py
@@ -0,0 +1,25 @@
+from collections import deque
+
+A, B, C = map(int, input().split())
+
+v = set()
+q = deque()
+node = tuple(sorted([A, B, C]))
+q.append(node)
+v.add(node)
+answer = 0
+
+while q:
+    a, b, c = q.popleft()
+
+    if a == b and b == c:
+        answer = 1
+        break
+
+    for x, y, z in [(a, b, c), (b, c, a), (a, c, b)]:
+        next = tuple(sorted([2 * x, y - x, z]))
+        if next not in v:
+            v.add(next)
+            q.append(next)
+
+print(answer)
