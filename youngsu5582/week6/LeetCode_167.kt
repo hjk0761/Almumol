0db0092f2be22bd7503f6abecba18ab6b26c6f30
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2025-02-28 22:54:14 +0900

    feat: 10주차 4차시

---
+++ youngsu5582/week6/LeetCode_167.kt
@@ -0,0 +1,16 @@
+package week6
+
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var left = 0
+    var right = numbers.size-1
+
+    while(left<right){
+        val sum = numbers[left]+ numbers[right]
+        if(sum == target){
+            return intArrayOf(left+1,right+1)
+        }
+        if(sum < target) left++
+        else right--
+    }
+    return intArrayOf(0,0)
+}
---
+++ youngsu5582/week6/LeetCode_55.kt
@@ -0,0 +1,11 @@
+package week6
+
+fun canJump(nums: IntArray): Boolean {
+    var last = nums.size - 1
+    for (i in nums.size - 2 downTo 0) {
+        if (i + nums[i] >= last) {
+            last = i
+        }
+    }
+    return last == 0
+}
