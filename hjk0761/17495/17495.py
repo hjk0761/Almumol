a1d20569d399ebbf79c66554b459b1bf3acae839
hjk0761 <hjk0761@naver.com>
2025-04-17 19:16:17 +0900

    feat: 누락된 파일 추가

--- hjk0761/17495/17495.py
+++ hjk0761/17495/17495.py
@@ -1,24 +1,55 @@
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, input().split())
+def parser(name):
+    s = {"C":0, "D":2, "E":4, "F":5, "G":7, "A":9, "B":11}
+    def base(n):
+        scale, height = n[0], n[1]
+        return 12*int(height) + s[scale]
+    if len(name) == 2:
+        return base(name)
+    else:
+        return base(name[:2]) + 1
 
-path = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
+MAX = 200_000
+MAX_SCALE = 121
+initial = list(input().strip().split())
+n = int(input().strip())
+sheet = list(input().strip().split())
 
-dp = [[[0 for _ in range(3)] for _ in range(m)] for _ in range(n)]
+def solve():
+    dp = [[[(MAX, (-1, -1)) for _ in range(MAX_SCALE)] for _ in range(MAX_SCALE)] for _ in range(n+1)]
+    l, r = parser(initial[0]), parser(initial[1])
+    dp[0][l][r] = (0, (-1, -1))
 
-for j in range(m):
-    for k in range(3):
-        dp[0][j][k] = path[0][j]
+    for i in range(n):
+        next = parser(sheet[i])
+        for j in range(MAX_SCALE):
+            for k in range(MAX_SCALE):
+                if dp[i][j][k][0] < MAX:
+                    left = dp[i][j][k][0] + abs(j - next)
+                    if dp[i+1][next][k][0] > left:
+                        dp[i+1][next][k] = (left, (j, k))
+                    right = dp[i][j][k][0] + abs(k - next)
+                    if dp[i+1][j][next][0] > right:
+                        dp[i+1][j][next] = (right, (j, k))
 
-for j in range(m):
-    dp[1][j][0] = sys.maxsize if j == 0 else dp[0][j-1][0] + path[1][j]
-    dp[1][j][1] = dp[0][j][1] + path[1][j]
-    dp[1][j][2] = sys.maxsize if j == m-1 else dp[0][j+1][2] + path[1][j]
+    result = MAX
+    prev = (-1, -1)
+    for i in range(MAX_SCALE):
+        for j in range(MAX_SCALE):
+            if dp[n][i][j][0] < result:
+                result = dp[n][i][j][0]
+                prev = (i, j)
+    print(result)
+    answer = []
+    for i in range(n, 0, -1):
+        if prev[0] == parser(sheet[i-1]):
+            answer.append(1)
+        else:
+            answer.append(2)
+        prev = dp[i][prev[0]][prev[1]][1]
 
-for i in range(2, n):
-    for j in range(m):
-        dp[i][j][0] = sys.maxsize if j == 0 else min(dp[i-1][j-1][1], dp[i-1][j-1][2]) + path[i][j]
-        dp[i][j][1] = min(dp[i-1][j][0], dp[i-1][j][2]) + path[i][j]
-        dp[i][j][2] = sys.maxsize if j == m-1 else min(dp[i-1][j+1][0], dp[i-1][j+1][1]) + path[i][j]
-
-print(min(min(line) for line in dp[n-1]))
+    answer.reverse()
+    print(*answer)
+solve()
