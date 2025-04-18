66af7fe93e6530294b287843823a3fea158cc06b
hjk0761 <hjk0761@naver.com>
2025-04-18 18:39:07 +0900

    feat: 누락된 파일 추가'

--- hjk0761/02565/2565.py
+++ hjk0761/02565/2565.py
@@ -1,26 +1,26 @@
 import sys
+input = sys.stdin.readline
 
-n = int(input())
-liquid = list(map(int, sys.stdin.readline().strip().split()))
+n = int(input().strip())
+wire = [list(map(int, input().strip().split())) for _ in range(n)]
+wire.sort(key = lambda x: x[0])
+dest = [x[1] for x in wire]
 
-left, right = 0, n-1
-result = sys.maxsize
-base, acid = -1, -1
-
-while left < right:
-    if liquid[left] > 0:
-        right = left + 1
-    elif liquid[right] < 0:
-        left = right - 1
-    mixed = liquid[left] + liquid[right]
-    if abs(mixed) < abs(result):
-        base, acid = left, right
-        result = mixed
-    if mixed == 0:
-        break
-    elif mixed > 0:
-        right -= 1
-    else:
-        left += 1
-
-print(liquid[base], liquid[acid])
+def find(i):
+    result = []
+    for j in range(i-1, -1, -1):
+        if dest[j] < dest[i]:
+            result.append(j)
+    return result
+dp = [1 for _ in range(n)]
+for i in range(n):
+    less = find(i)
+    for j in less:
+        dp[i] = max(dp[i], dp[j] + 1)
+wire.reverse()
+dp1 = [1 for _ in range(n)]
+for i in range(n):
+    less = find(i)
+    for j in less:
+        dp1[i] = max(dp1[i], dp1[j] + 1)
+print(n - max(max(dp), max(dp1)))
