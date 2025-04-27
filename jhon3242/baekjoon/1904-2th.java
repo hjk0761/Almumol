f28319ba220b512d367d53fbad5cb69f35ec9b88
Wonjun Choi(타칸) <78288539+jhon3242@users.noreply.github.com>
2025-04-27 11:28:27 +0700

    Create 1904-2th.java

---
+++ jhon3242/baekjoon/1904-2th.java
@@ -0,0 +1,58 @@
+package backjoon.n1904;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+
+00
+11
+2
+
+111
+100
+001
+3
+
+0000
+0011
+1001
+1100
+1111
+5
+
+11111
+11100
+11001
+10011
+00111
+10000
+00001
+00100
+8
+
+
+5C0 + 4C1 + 3C2
+
+111111
+6C0 + 5C1 + 4C2 + 3C3
+
+1111111
+7C0 + 6C1 + 5C2 + 4C3
+
+...
+
+NC0 + N-1C1 ....
+
+ */
+
+public class Main2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
