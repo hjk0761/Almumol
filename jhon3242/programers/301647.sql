d1e48662a81f4ae6a6cbec33ebc404136d0d0766
Wonjun Choi <78288539+jhon3242@users.noreply.github.com>
2025-08-27 10:23:19 +0900

    Create 301647.sql

---
+++ jhon3242/programers/301647.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+SELECT child.ID AS ID, child.GENOTYPE AS GENOTYPE, parent.GENOTYPE AS PARENT_GENOTYPE
+FROM ECOLI_DATA AS child
+LEFT JOIN ECOLI_DATA AS parent
+    ON child.PARENT_ID = parent.ID
+WHERE parent.GENOTYPE & child.GENOTYPE >= parent.GENOTYPE
+ORDER BY child.ID
+;
